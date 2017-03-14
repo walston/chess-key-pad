097a347b749c8d11c07da7a61aff0088e6fb8483
walston <nathanielwalston@gmail.com>
2017-03-14 08:28:44 -0700

    Completely refactor the way data is stored
    Rather than store data in an Array, create a simple Trie structure that expands itself recursively.
    
    Rather than print everything as a huge concatted string from that array, use a callback.

--- getPhoneNumbers.js
+++ getPhoneNumbers.js
@@ -3,29 +3,85 @@ const _ = require('underscore')
 module.exports = function (options) {
   const { width, height, length, keys, illegalPlacement, pieces } = options
   const getMoves = require('./getMoves')(width,height,keys,illegalPlacement)
-  const unfolds = {}
+  const unfolds = pieces.reduce( (dictionary, p) => {
+    dictionary[p] = keys.reduce( (moves, k, i) => {
+      moves[k] = getMoves(p, i)
+      return moves
+    }, {})
+    return dictionary
+  }, {})
 
-  return expand;
+  function Node(char, piece) {
+    this.char = char
+    this.depth = 0
+    this.children = {}
+    this.parents = []
+    this.piece = piece
+  }
 
-  function expand(piece, digits) {
-    if (!digits.slice) return []
-    if (digits[0] == "0" || digits[0] == "1") return []
-    if (!unfolds[piece]) unfolds[piece] = keys.map((k,i) => getMoves(piece,i))
+  Node.prototype = {
+    expand: function() {
+      let expansions = unfolds[this.piece]
+      let letter = this.char.slice(-1)
+      if (expansions[letter] && this.parents.length < length) {
+        this.children = expansions[letter].reduce( (acc, k) => {
+          acc[k] = new Node(k, this.piece)
+          return acc
+        }, {})
+      }
+      for (let name in this.children) {
+        let child = this.children[name]
+        child.depth = this.depth + 1
+        child.parents = this.parents.concat(this.char)
+        child.expand()
+      }
+    },
+    print: function(printer) {
+      let children = false
+      for (let name in this.children) {
+        let child = this.children[name]
+        child.print(printer)
+        children = true
+      }
 
-    if (digits.length >= length) return digits
-
-    let lastTile = keys.indexOf(digits.slice(-1))
-    let expansion = []
-
-    // promote a pawn!
-    if (piece == 'pawn' && lastTile == 10) {
-      piece = 'queen'
-      expansion = getMoves(piece, lastTile).map( k => expand(piece, digits + k) )
+      if (!children) {
+        if (this.parents.length >= length - 3) debugger;
+        printer(this.parents.join('') + this.char)
+      }
     }
-    else {
-      expansion = unfolds[piece][lastTile].map( k => expand(piece, digits + k) )
-    }
+  }
 
-    return expansion.reduce( (acc, digits) => acc.concat(digits), [] )
+  function Trie(piece, seed) {
+    if (!seed || !piece) { return console.log('No seed or piece supplied') }
+
+    this.root = new Node('', piece)
+    this.tail = this.root
+
+    let nodes = seed.split('').reduce( (stem, k) => {
+      let node = new Node(k)
+      stem.children[k] = node
+      node.parents = stem.parents.concat(stem.char)
+      node.depth = stem.depth + 1
+      node.piece = stem.piece
+
+      this.tail = node
+      return node
+    }, this.root)
+  }
+
+  Trie.prototype = {
+    expand: function() {
+      this.tail.expand()
+    },
+    print: function(printer) {
+      this.tail.print(printer)
+    }
+  }
+
+  return function(piece, seed) {
+    let trie = new Trie(piece, seed)
+    console.log(trie);
+    trie.expand()
+    return trie
   }
 }
